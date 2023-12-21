fd860e446ec9fc7a4305575e85b9be8d097b2923
CodePapayas <catspianos@gmail.com>
2023-12-20 16:31:27 -0800

    Add search and edit functions

--- onboard.py
+++ onboard.py
@@ -25,8 +25,9 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("Employee Onboarding")
-        self.root.geometry("250x500")
+        self.root.geometry("500x500")
         self.root.configure(bg='#B2AC88')  # Set background color to sage green
+        self.current_employee_id = None  # Set employee ID for search/edit
 
         # Define a custom font style for the application
         custom_font = ("Times New Roman", 10)  # Example: Using Times New Roman
@@ -76,6 +77,96 @@ class App:
         self.confirm_button = tk.Button(root, text="Confirm", command=self.confirm, bg='#B2AC88', font=custom_font)
         self.confirm_button.pack(anchor='w', padx=10, pady=10)
 
+        # Search button
+        self.search_button = tk.Button(root, text="Search", command=self.read_records, bg='#B2AC88', font=custom_font)
+        self.search_button.pack(anchor='w', padx=10, pady=5)
+
+        # Edit button  
+        self.edit_button = tk.Button(root, text="Edit", command=self.edit_records, bg='#B2AC88', font=custom_font)
+        self.edit_button.pack(anchor='w', padx=10, pady=5)
+
+        # Search entry box
+        self.search_entry = tk.Entry(root, font=custom_font)
+        self.search_entry.pack(anchor='w', padx=10, pady=5)
+
+        # Display for search results
+        self.employee_listbox = tk.Listbox(self.root, font=custom_font)
+        self.employee_listbox.pack(anchor='w', padx=25, pady=25)
+
+    def edit_records(self):
+        selected_index = self.employee_listbox.curselection()
+        if not selected_index:
+            print("No item selected")
+            return
+        
+        employee_id = self.employee_listbox.get(selected_index[0])[0]
+
+        try:
+            with sqlite3.connect('employee_data.db') as conn:
+                cur = conn.cursor()
+                cur.execute("SELECT * FROM employees WHERE id = ?", (employee_id,))
+                employee_data = cur.fetchone()
+
+                #Populate the fields
+                self.name_entry.delete(0, tk.END)
+                self.name_entry.insert(0, employee_data[1])
+                print(employee_data[1])
+
+                self.start_date_entry.delete(0, tk.END)
+                self.start_date_entry.insert(0, employee_data[2])
+                print(employee_data[2])
+
+                self.title_entry.delete(0, tk.END)
+                self.title_entry.insert(0, employee_data[3])
+                print(employee_data[3])
+
+                self.department_var.set(employee_data[4])
+                print(employee_data[4])
+
+                self.check_var.set(employee_data[5])
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+
+    def read_records(self):
+        search_query = self.search_entry.get()
+        self.employee_listbox.delete(0, tk.END)
+
+        try:
+            with sqlite3.connect('employee_data.db') as conn:
+                cur = conn.cursor()
+                query = "SELECT * FROM employees WHERE name LIKE ?"
+                cur.execute(query, ('%' + search_query + '%',))
+                rows = cur.fetchall()
+                for row in rows:
+                    self.employee_listbox.insert(tk.END, row)
+            if rows:
+                self.employee_listbox.pack(anchor='w', padx=10, pady=10)  # Show the Listbox
+            else:
+                self.employee_listbox.pack_forget()  # Hide the Listbox if no results
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+
+    def refresh_employee_list(self):
+        self.employee_listbox.delete(0, tk.END)
+
+        try:
+            with sqlite3.connect('employee_data.db') as conn:
+                cur = conn.cursor()
+                cur.execute("SELECT * FROM employees ORDER BY id")
+                rows = cur.fetchall()
+                for row in rows:
+                    self.employee_listbox.insert(tk.END, row)
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+
+    def clear_fields(self):
+        # Method to clear all input fields
+        self.name_entry.delete(0, tk.END)
+        self.start_date_entry.delete(0, tk.END)
+        self.title_entry.delete(0, tk.END)
+        self.department_var.set(self.departments[0])
+        self.check_var.set(0)  
+
     def confirm(self):
         name = self.name_entry.get()
         start_date = self.start_date_entry.get()
@@ -88,15 +179,28 @@ class App:
         try:
             conn = sqlite3.connect('employee_data.db')
             cur = conn.cursor()
-            cur.execute('''
-                INSERT INTO employees (name, start_date, title, department, cell_phone_provided)
-                VALUES (?, ?, ?, ?, ?)
-            ''', (name, start_date, title, department, cell_phone == "Yes"))
+
+            if self.current_employee_id is None:
+                cur.execute('''
+                    INSERT INTO employees (name, start_date, title, department, cell_phone_provided)
+                    VALUES (?, ?, ?, ?, ?)
+                ''', (name, start_date, title, department, cell_phone == "Yes"))
+            else:
+                cur.execute('''
+                UPDATE employees
+                SET name = ?, start_date = ?, title = ?, department = ?, cell_phone_provided = ?
+                WHERE id = ?
+                '''), (name, start_date, title, department, cell_phone == "Yes", self.current_employee_id)
+                self.current_employee_id = None
+
             conn.commit()
         except sqlite3.Error as e:
             print(f"An error occurred: {e.args[0]}")
         finally:
             conn.close()
+
+        self.clear_fields()
+        self.refresh_employee_list()
 
         create_interactive_pdf(name, start_date, title, department, cell_phone, tasks)
 
---
+++ test.py
@@ -0,0 +1,7 @@
+x = 10
+y = x
+z = y
+
+x = x + 1
+
+print(x, y, z)
